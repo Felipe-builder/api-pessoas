a43c7d2bfc00f9604b7ddb71ec48399e15d070eb
Felipe-builder <felipesvascon@gmail.com>
2022-06-07 23:29:49 -0300

    refreshToken

--- src/models/Token.js
+++ src/models/Token.js
@@ -1,4 +1,5 @@
 import  jwt  from "jsonwebtoken";
+import crypto from "crypto";
 
 export default class Token {
     static criarTokenJWT(usuario) {
@@ -7,5 +8,10 @@ export default class Token {
         }
         const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m'});
         return token
+    }
+
+    static criaTokenOpaco() {
+        const tokenOpaco = crypto.randomBytes(24).toString('hex');
+        return tokenOpaco;
     }
 }
